d83caa99e2d04022a72b7124437fd3018d9444c7
TTAR007 <tar127290@gmail.com>
2025-07-27 16:51:58 +0700

    finish

---
+++ app.js
@@ -0,0 +1,56 @@
+const ratingBtns = document.querySelectorAll(".rating");
+
+for (const rating of ratingBtns) {
+    rating.addEventListener("click", (eventObj) => {
+        // before click, remove style of another element that we have added first.
+        // loop through all elements, but removeAttribute() if specified attribute does not exist It will not generate an error
+        ratingBtns.forEach((rating) => {
+            rating.removeAttribute("style");
+        })
+
+        // add style to rating button
+        eventObj.currentTarget.style.color = "var(--Grey-950)";
+        eventObj.currentTarget.style.backgroundColor = "var(--Orange)";
+    })
+}
+
+const submitBtn = document.getElementById("submit-btn");
+const h1 = document.getElementById("h1");
+const description = document.getElementById("description-text");
+const star = document.getElementById("star");
+const ratings = document.getElementById("ratings");
+const phoneImg = document.getElementById("phone");
+const resultText = document.getElementById("result");
+
+submitBtn.addEventListener("click", () => {
+    // get a rating that user have selected
+    // if a rating button has "style" attribute, it means it has been selected.
+    let selectedRate = 0;
+    for (const rating of ratingBtns) {
+        if (rating.hasAttribute("style")) {
+            selectedRate = rating.textContent;
+            break;
+        }
+    }
+
+    if (selectedRate === 0) {
+        alert("Please select one");
+    } else {
+        // change text to thank you
+        h1.textContent = "Thank you!";
+        description.textContent = "We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!";
+        h1.classList.add("text-center");
+        description.classList.add("text-center");
+    
+        //display mobile img and rating result
+        phoneImg.classList.remove("hide");
+        resultText.classList.remove("hide");
+        resultText.textContent = `You selected ${selectedRate} out of 5`;
+    
+        //display none of previous elements
+        star.classList.add("hide");
+        ratings.classList.add("hide");
+        submitBtn.classList.add("hide");
+    }
+
+})
